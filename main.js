6a25f99d6fbce4761bfa3c5b4753ac645e5067f8
irakli85 <iberidze38@gmail.com>
2022-08-23 16:33:00 +0400

    some changes

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 let billInput = document.querySelector("#bill");
 let percentageInput = document.querySelectorAll(".tips");
 let customInput = document.querySelector("#percentage-input");
-let peopleInput = document.querySelector("#people")
+let peopleInput = document.querySelector("#people");
+let tipAmount = document.getElementById("#tip-per-person");
 
 
 // default values
@@ -35,8 +36,12 @@ function peopleInputFn(){
 }
 
 
+
 //event listeners
 billValue = addEventListener("input", billInputFn);
 percentageValue = addEventListener("click", percentageInputFn);
 customValue = addEventListener("input", customInputFn);
 peopleValue = addEventListener("input", peopleInputFn);
+
+
+
