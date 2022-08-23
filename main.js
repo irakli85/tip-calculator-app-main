bb8e99f382dcc9925dacc22869675fddd6269cfe
irakli85 <iberidze38@gmail.com>
2022-08-23 15:48:40 +0400

    some changes

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // variables
 let billInput = document.querySelector("#bill");
 let percentageInput = document.querySelectorAll(".tips");
+let customInput = document.querySelector("#percentage-input");
+let peopleInput = document.querySelector("#people")
 
 
 // default values
 let billValue = 0;
+let customValue = 0;
+let peopleValue = 1;
 
 //functions
 function billInputFn(){
@@ -17,11 +21,22 @@ function percentageInputFn(){
         if (tipList.checked) {
           console.log(tipList.value)
         }
-      }
-    
+      }    
+}
+
+function customInputFn(){
+    customValue = customInput.value / 100;
+    console.log(customValue);
+}
+
+function peopleInputFn(){
+    peopleValue = peopleInput.value;
+    console.log(peopleValue);
 }
 
 
 //event listeners
 billValue = addEventListener("input", billInputFn);
 percentageValue = addEventListener("click", percentageInputFn);
+customValue = addEventListener("input", customInputFn);
+peopleValue = addEventListener("input", peopleInputFn);
