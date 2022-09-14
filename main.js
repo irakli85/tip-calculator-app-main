1b69b51862d4cbff46102b058331f195260447f9
irakli85 <iberidze38@gmail.com>
2022-09-14 16:14:46 +0400

    some changes

--- main.js
+++ main.js
@@ -5,39 +5,53 @@ let customInput = document.querySelector("#percentage-input");
 let peopleInput = document.querySelector("#people");
 let tipAmount = document.querySelector("#tip-per-person");
 let totalAmount = document.querySelector("#total-per-person");
+let resetAll = document.querySelector("#reset");
 
 
 // default values
-let billValue = 0;
-let customValue = 0;
-let peopleValue = 1;
-let percentageValue = 1;
+// let billValue = 0;
+// let customValue = 0;
+// let peopleValue = 1;
+// let percentageValue = 1;
 
 
 //functions
 
 function billInputFn(){
-    billValue = billInput.value;    
-    console.log(billValue);
+    billValue = Number(billInput.value);    
+    // console.log(billValue);
 }
 
 function percentageInputFn(){
     for (let tipList of percentageInput) {
         if (tipList.checked) {
-          console.log(tipList.value)
+        //   console.log(tipList.value)
+        percentageValue = Number(tipList.value);
         }
       }    
 }
 
 function customInputFn(){
-    customValue = customInput.value / 100;
-    console.log(customValue);
+    customValue = Number(customInput.value / 100);
+    // console.log(customValue);
 }
 
-function peopleInputFn(){
-    peopleValue = peopleInput.value;    
-    console.log(peopleValue);
+function resetFn(){
+    billInput.value = 0;
+    peopleInput.value = 1;
+    tipAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
 }
+
+
+function peopleInputFn(){
+    peopleValue = Number(peopleInput.value);    
+    // console.log(peopleValue);
+    tipAmount.innerText = (billValue * percentageValue) / peopleValue;
+    totalAmount.innerText = (billValue + (billValue * percentageValue)) / peopleValue;
+}
+
+
 
 
 
@@ -47,6 +61,7 @@ billValue = addEventListener("input", billInputFn);
 percentageValue = addEventListener("click", percentageInputFn);
 customValue = addEventListener("input", customInputFn);
 peopleValue = addEventListener("input", peopleInputFn);
+resetAll.addEventListener("click", resetFn);
 
 
 
